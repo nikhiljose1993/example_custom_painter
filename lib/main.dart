131515aa3dff2b23f1ef3989477aa105ceafbc7d
Nikhil <nikhiljose18@gmail.com>
2024-05-13 10:57:55 +0530

    Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:example_custom_painter/recipes_bloc/recipe_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -47,21 +49,68 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(child: BlocBuilder<RecipeBloc, RecipeState>(
-        builder: (context, state) {
-          return ListView.builder(
-            itemCount: state.rList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage('${state.rList[index]!.image}'),
+      body: Center(
+        child: BlocBuilder<RecipeBloc, RecipeState>(
+          builder: (context, state) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: GridView.builder(
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  childAspectRatio: 9 / 15,
                 ),
-                title: Text('${state.rList[index]!.name}'),
-              );
-            },
-          );
-        },
-      )),
+                itemCount: state.rList.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: const Color.fromRGBO(248, 209, 255, 1),
+                    ),
+                    child: Stack(
+                      children: [
+                        Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(6),
+                              child: Image.network(
+                                '${state.rList[index]!.image}',
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 4),
+                              child: Text(
+                                '${state.rList[index]!.name}',
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '${state.rList[index]!.cuisine}',
+                            ),
+                            Text(
+                              '${state.rList[index]!.difficulty}',
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
+//
